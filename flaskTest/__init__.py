4a84a2a5d161bc763199c2f7df2c8fc4942d70ee
Nyatichi Kenani <kenanitichi@gmail.com>
2020-08-28 00:09:45 -0500

    bug fixes

--- flaskTest/__init__.py
+++ flaskTest/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
